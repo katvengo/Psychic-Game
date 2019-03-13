7d924f421285ec71872ee590470b96a988011a8e
Kat Vengo <katvengo@gmail.com>
2019-03-12 21:24:13 -0700

    finished with read me

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+This project allows us to play a game in which the user tries to guess what the computer has chosen as a letter. You get 9 tries and once those are up, we increase our losses by 1. If you win, we increase wins by 1.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,50 @@
-var choices = ['a','b','c','d', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
- 
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 var winner = 0;
 var losses = 0;
 var guessesLeft = 9;
 var soFar = [];
 
 
- var wins = document.getElementById("win");
- var loss = document.getElementById('losses');
- var guessesSoFar = document.getElementById('sofar');
- var guessesLeftToUse = document.getElementById('left');
+var wins = document.getElementById("win");
+var loss = document.getElementById('losses');
+var guessesSoFar = document.getElementById('sofar');
+var guessesLeftToUse = document.getElementById('left');
 
- function reset () {
+function reset() {
   guessesLeftToUse.textContent = 9;
   soFar = [];
-   }
+}
 
-  function userGuesses() {
-    // for (i = 0; i < soFar.length; i++) {
-    //   GuessesSoFar.textContent = soFar[i];
-      
-    // }
-    console.log(soFar)
-    guessesSoFar.innerHTML = soFar;
+function userGuesses() {
+  console.log(soFar)
+  guessesSoFar.innerHTML = soFar;
+}
+
+document.onkeyup = function (event) {
+  var userKey = event.key;
+  console.log(userKey);
+  var computerLetter = choices[Math.floor(Math.random() * choices.length)];
+
+
+  if (userKey === computerLetter) {
+    wins.textContent++;
+    console.log('you won!');
+    reset()
+
   }
 
-document.onkeyup = function(event) {
-  var userKey = event.key;
-  console.log (userKey);
-  var computerLetter = choices[Math.floor(Math.random() * choices.length)];
- 
-
-   if (userKey === computerLetter){
-     wins.textContent ++;
-     console.log('you won!');
-       if (parseInt(guessesLeftToUse.textContent) === 0)  {
-         reset()
-       }
-   } 
-    
-   if(userKey !== computerLetter) {
+  if (userKey !== computerLetter) {
     guessesSoFar.textContent = event.key;
     console.log('you lost!');
-    guessesLeftToUse.textContent --;
+    guessesLeftToUse.textContent--;
     soFar.push(guessesSoFar.textContent);
     userGuesses()
     if (parseInt(guessesLeftToUse.textContent) === 0) {
-      loss.textContent ++;
+      loss.textContent++;
       reset()
-      
+
+
     }
-   }
   }
+}
